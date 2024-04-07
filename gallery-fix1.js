04a8b32e563b71053af86833290ee0e482a42203
nate <nate@nateui-MacBookAir.local>
2024-04-07 10:05:01 +0900

    공유하기까지 작업 진행중

--- gallery-fix1.js
+++ gallery-fix1.js
@@ -7,8 +7,15 @@ window.onload = () => {
     hiddenImages.forEach((img) => {
       img.classList.remove("hidden");
     });
-    showMoreBtn.style.display = "none";
     collapseBtn.style.display = "inline-block";
+    collapseBtn.style.backgroundColor = "ffe08c";
+    collapseBtn.style.width = "70px";
+    collapseBtn.style.textAlign = "center";
+    collapseBtn.style.display = "block";
+    collapseBtn.style.margin = "7% auto";
+    collapseBtn.style.borderRadius = "15px";
+
+    showMoreBtn.style.display = "none";
   });
 
   collapseBtn.addEventListener("click", () => {
@@ -17,7 +24,14 @@ window.onload = () => {
       img.classList.add("hidden");
     });
     collapseBtn.style.display = "none";
+
     showMoreBtn.style.display = "inline-block";
+    showMoreBtn.style.backgroundColor = "ffe08c";
+    showMoreBtn.style.width = "70px";
+    showMoreBtn.style.textAlign = "center";
+    showMoreBtn.style.display = "block";
+    showMoreBtn.style.margin = "7% auto";
+    showMoreBtn.style.borderRadius = "15px";
   });
 
   const galleryImages = document.querySelectorAll("#gallery img");
