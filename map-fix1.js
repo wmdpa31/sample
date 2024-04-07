5bc674984531f173122966686fb8bd39378411b6
jikang <g2ang12@gmail.com>
2024-04-07 21:24:41 +0900

    Feat: 카카오맵에 가이드 문구 추가

---
+++ map-fix1.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mapContainerElement = document.getElementById("map");
+  
+  const guideDiv = document.createElement("div");
+  const spanElement = document.createElement("span");
+  spanElement.style.color = "#000";
+  spanElement.style.fontSize = "0.7rem";
+  spanElement.style.opacity = 1;
+  spanElement.textContent = "목적지 클릭하면 크게 볼 수 있습니다.";
+
+  guideDiv.appendChild(spanElement);
+
+  guideDiv.id = "map-guide";
+  guideDiv.style.bottom = 0;
+  guideDiv.style.height = "30px";
+  guideDiv.style.backgroundColor = "rgba(220, 210, 180, 0.5)";
+  guideDiv.style.width = "100%";
+  guideDiv.style.opacity = 0.5;
+  guideDiv.style.position = 'absolute';
+  guideDiv.className = "flex justify-center items-center"
+  
+  mapContainerElement.appendChild(guideDiv);
+});
+
