1df7dfe9232dbf711dfb3993efbd31e7b55a3437
nate <nate@nateui-MacBookAir.local>
2024-04-07 10:26:07 +0900

    카카오 공유 수정

--- gallery-fix1.js
+++ gallery-fix1.js
@@ -40,4 +40,62 @@ window.onload = () => {
       img.classList.toggle("full");
     };
   });
+
+  const btnShareKa = document.querySelector(".shareKt");
+  btnShareKa.addEventListener("click", () => {
+    // Kakao.Share.sendDefault({
+    //   objectType: "text",
+    //   text: "김동동 ♥ 오아리의 결혼식에 초대합니다.",
+    //   link: {
+    //     mobileWebUrl: "https://mo1.luvle.co.kr",
+    //     webUrl: "https://mo1.luvle.co.kr",
+    //   },
+    // });
+
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "김동동 ♥ 오아리의 모바일 청첩장",
+        description: "김동동 ♥ 오아리의 결혼식에 초대합니다.",
+        imageUrl: "img_2/01.png",
+        link: {
+          mobileWebUrl: "https://mo1.luvle.co.kr",
+          webUrl: "https://mo1.luvle.co.kr",
+        },
+      },
+      buttons: [
+        {
+          title: "웹으로 보기",
+          link: {
+            mobileWebUrl: "https://mo1.luvle.co.kr",
+            webUrl: "https://mo1.luvle.co.kr",
+          },
+        },
+      ],
+      // 카카오톡 미설치 시 카카오톡 설치 경로이동
+      installTalk: true,
+    });
+  });
+
+  const btnShareKs = document.querySelector(".shareKs");
+  btnShareKs.addEventListener("click", () => {
+    console.log("111");
+    Kakao.Story.open({
+      url: "https://developers.kakao.com",
+      text: "카카오 개발자 사이트로 놀러오세요! #개발자 #카카오 :)",
+    });
+  });
+
+  const btnShareFb = document.querySelector(".shareFb");
+  btnShareFb.addEventListener("click", () => {
+    const pageUrl = "mo1.luvle.co.kr/";
+    window.open(`http://www.facebook.com/sharer/sharer.php?u=${pageUrl}`);
+  });
+
+  const shareViaSMSBtn = document.querySelector(".shareTwoG");
+  shareViaSMSBtn.addEventListener("click", () => {
+    const message = "모바일 청첩장을 확인해주세요! " + window.location.href;
+    const link = "sms:?body=" + encodeURIComponent(message);
+    window.location.href = link;
+  });
 };
